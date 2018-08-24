599b47269d2179703eefaff5196dd90320267105
Samuel <smasson99@gmail.com>
2018-08-24 17:11:58 -0400

    Background fonctionnel

---
+++ PracticeProject/New Unity Project/Assets/Scripts/OnClickSoundScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickSoundScript : MonoBehaviour
+{
+    [Tooltip("The sound to play on click.")]
+    [SerializeField]
+    private AudioClip soundToPlay = null;
+
+    private Button button;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        gameObject.AddComponent<AudioSource>();
+
+        audioSource = GetComponent<AudioSource>();
+        button = GetComponent<Button>();
+
+        audioSource.clip = soundToPlay;
+        audioSource.playOnAwake = false;
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(soundToPlay);
+    }
+}
--- PracticeProject/New Unity Project/Assets/Scripts/SpriteSlider.cs
+++ PracticeProject/New Unity Project/Assets/Scripts/SpriteSlider.cs
@@ -1,149 +1,149 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpriteSlider : MonoBehaviour
 {
-    [Tooltip("Draw here your sprites.")]
-    [SerializeField]
-    private List<Sprite> spritesToSlide = new List<Sprite>();
+  [Tooltip("Draw here your sprites.")]
+  [SerializeField]
+  private List<Sprite> spritesToSlide = new List<Sprite>();
 
-    [Tooltip("Do you want the sprites to switch randomly?")]
-    [SerializeField]
-    private bool slideSpritesRandomly = true;
+  [Tooltip("Do you want the sprites to switch randomly?")]
+  [SerializeField]
+  private bool slideSpritesRandomly = true;
 
-    [Tooltip("How much time must remain between each slides?")]
-    [SerializeField]
-    private float sliderDelay = 10.5f;
+  [Tooltip("How much time in seconds must remain between each slides?")]
+  [SerializeField]
+  private float sliderDelay = 10.5f;
 
-    [Tooltip("Only one of the two components must have this checked.")]
-    [SerializeField]
-    private bool isTheActiveSlideComponent = false;
+  [Tooltip("Only one of the two components must have this checked.")]
+  [SerializeField]
+  private bool isTheActiveSlideComponent = false;
 
-    [Tooltip("Set this serializable only if isTheSliderComponent is checked.")]
-    [SerializeField]
-    private Animator spriteSliderAnimator = null;
+  [Tooltip("Set this serializable only if isTheSliderComponent is checked.")]
+  [SerializeField]
+  private Animator spriteSliderAnimator = null;
 
-    [Tooltip("Set this serializable only if isTheSliderComponent is checked.")]
-    [SerializeField]
-    private GameObject inactiveSpriteSliderGameObject = null;
+  [Tooltip("Set this serializable only if isTheSliderComponent is checked.")]
+  [SerializeField]
+  private GameObject inactiveSpriteSliderGameObject = null;
 
-    private SpriteRenderer spriteRenderer;
-    private SpriteSlider inactiveSpriteSlider;
-    private int curIndex;
-    private float lastReset;
-    private bool fadesOut;
-    private bool hasChanged;
-    private bool sliderIsPaused;
+  private Image image;
+  private SpriteSlider inactiveSpriteSlider;
+  private int curIndex;
+  private float lastReset;
+  private bool fadesIn;
+  private bool hasChanged;
+  private bool sliderIsPaused;
 
-    private void Awake()
+  private void Awake()
+  {
+    image = GetComponent<Image>();
+
+    if (isTheActiveSlideComponent)
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+      inactiveSpriteSlider = inactiveSpriteSliderGameObject.GetComponentInChildren<SpriteSlider>();
 
-        if (isTheActiveSlideComponent)
+      if (inactiveSpriteSlider == null)
+      {
+        Debug.Log("The SpriteSlider in the serializable was not found! Please fix this!");
+      }
+    }
+
+    if (image == null)
+    {
+      Debug.Log("The image was not found! Please fix this!");
+    }
+  }
+
+  private void Start()
+  {
+    curIndex = -1;
+    fadesIn = true;
+
+    ResetSliderDelay();
+    UpdateSlide();
+  }
+
+  private void Update()
+  {
+    if (!sliderIsPaused && SliderDelayPassed())
+    {
+      sliderIsPaused = true;
+      Slide();
+    }
+  }
+
+  private void ChangeSlideIndex()
+  {
+    if (image != null)
+    {
+      if (slideSpritesRandomly)
+      {
+        int lastIndex = curIndex;
+        curIndex = (int)Random.Range(0, spritesToSlide.Count);
+
+        while (curIndex == lastIndex)
         {
-            inactiveSpriteSlider = inactiveSpriteSliderGameObject.GetComponentInChildren<SpriteSlider>();
-
-            if (inactiveSpriteSlider == null)
-            {
-                Debug.Log("The SpriteSlider in the serializable was not found! Please fix this!");
-            }
+          curIndex = (int)Random.Range(0, spritesToSlide.Count);
         }
-
-        if (spriteRenderer == null)
+      }
+      else
+      {
+        ++curIndex;
+        if (curIndex > spritesToSlide.Count)
         {
-            Debug.Log("The sprite component wasn't found! Please fix this!");
+          curIndex = 0;
         }
+      }
     }
+  }
 
-    private void Start()
+  public void UpdateSlide()
+  {
+    ChangeSlideIndex();
+    image.sprite = spritesToSlide.ToArray()[curIndex];
+  }
+
+  private bool SliderDelayPassed()
+  {
+    return (lastReset + sliderDelay) <= Time.time;
+  }
+
+  private void ResetSliderDelay()
+  {
+    lastReset = Time.time;
+    sliderIsPaused = false;
+  }
+
+  private void Slide()
+  {
+    if (isTheActiveSlideComponent)
     {
-        curIndex = -1;
-        fadesOut = true;
-        sliderIsPaused = false;
-
-        ResetSliderDelay();
-        UpdateSlide();
+      spriteSliderAnimator.SetBool("CanFadeOut", fadesIn == false);
+      spriteSliderAnimator.SetBool("CanFadeIn", fadesIn == true);
+      fadesIn = !fadesIn;
     }
+  }
 
-    private void Update()
-    {
-        if (!sliderIsPaused && SliderDelayPassed())
-        {
-            sliderIsPaused = true;
-            Slide();
-        }
-    }
+  public void NotifyCanChange()
+  {
+    CleanAnimator();
+    UpdateSlide();
+    ResetSliderDelay();
+  }
 
-    private void ChangeSlideIndex()
-    {
-        if (spriteRenderer != null)
-        {
-            if (slideSpritesRandomly)
-            {
-                int lastIndex = curIndex;
-                curIndex = (int)Random.Range(0, spritesToSlide.Count);
+  public void NotifyTwinSlider()
+  {
+    CleanAnimator();
+    inactiveSpriteSlider.UpdateSlide();
+    ResetSliderDelay();
+  }
 
-                while (curIndex == lastIndex)
-                {
-                    curIndex = (int)Random.Range(0, spritesToSlide.Count);
-                }
-            }
-            else
-            {
-                ++curIndex;
-                if (curIndex > spritesToSlide.Count)
-                {
-                    curIndex = 0;
-                }
-            }
-        }
-    }
-
-    public void UpdateSlide()
-    {
-        ChangeSlideIndex();
-        spriteRenderer.sprite = spritesToSlide.ToArray()[curIndex];
-    }
-
-    private bool SliderDelayPassed()
-    {
-        return (lastReset + sliderDelay) <= Time.time;
-    }
-
-    private void ResetSliderDelay()
-    {
-        lastReset = Time.time;
-        sliderIsPaused = false;
-    }
-
-    private void Slide()
-    {        
-        if (isTheActiveSlideComponent)
-        {
-            spriteSliderAnimator.SetBool("CanFadeOut", fadesOut == true);
-            spriteSliderAnimator.SetBool("CanFadeIn", fadesOut == false);
-            fadesOut = !fadesOut;
-        }
-    }
-
-    public void NotifyCanChange()
-    {
-        CleanAnimator();
-        UpdateSlide();
-        ResetSliderDelay();
-    }
-
-    public void NotifyTwinSlider()
-    {
-        CleanAnimator();
-        inactiveSpriteSlider.UpdateSlide();
-        ResetSliderDelay();
-    }
-
-    private void CleanAnimator()
-    {
-        spriteSliderAnimator.SetBool("CanFadeIn", false);
-        spriteSliderAnimator.SetBool("CanFadeOut", false);
-    }
+  private void CleanAnimator()
+  {
+    spriteSliderAnimator.SetBool("CanFadeIn", false);
+    spriteSliderAnimator.SetBool("CanFadeOut", false);
+  }
 }
